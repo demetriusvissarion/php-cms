aacdc96c36b9a1656d86993c5927ab7342a5624b
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-04-19 12:24:50 +0100

    established database connection page1

--- app/views/page1.view.php
+++ app/views/page1.view.php
@@ -3,10 +3,16 @@
 <h1>Page 1</h1>
 
 <?php 
-    require_once 'core/database/QueryBuilder.php';
-    $pdo = new PDO('mysql:host=your_host;dbname=your_db;charset=utf8', 'your_username', 'your_password');
-    $db = new QueryBuilder($pdo);
-    $page1Content = $db->selectAll('pages')[0]->page1_content;
+require_once 'core/database/QueryBuilder.php';
+$config = require 'config.php';
+$pdo = new PDO(
+    $config['database']['connection'] . ';dbname=' . $config['database']['name'] . ';charset=utf8',
+    $config['database']['username'],
+    $config['database']['password'],
+    $config['database']['options']
+);
+$db = new QueryBuilder($pdo);
+$page1Content = $db->selectAll('page1')[0]->page1_text;
 ?>
 
 <p><?php echo $page1Content; ?></p>

ae41a1d9e22d75e23220c37ad2a82ae0c0970f32
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-04-19 12:27:40 +0100

    pages 2 and 3 database connection

--- app/views/page2.view.php
+++ app/views/page2.view.php
@@ -2,5 +2,20 @@
 
 <h1>Page 2</h1>
 
+<?php 
+require_once 'core/database/QueryBuilder.php';
+$config = require 'config.php';
+$pdo = new PDO(
+    $config['database']['connection'] . ';dbname=' . $config['database']['name'] . ';charset=utf8',
+    $config['database']['username'],
+    $config['database']['password'],
+    $config['database']['options']
+);
+$db = new QueryBuilder($pdo);
+$page2Content = $db->selectAll('page2')[0]->page2_text;
+?>
+
+<p><?php echo $page2Content; ?></p>
+
 <?php require('partials/footer.php') ?>
 
--- app/views/page3.view.php
+++ app/views/page3.view.php
@@ -2,4 +2,19 @@
 
 <h1>Page 3</h1>
 
+<?php 
+require_once 'core/database/QueryBuilder.php';
+$config = require 'config.php';
+$pdo = new PDO(
+    $config['database']['connection'] . ';dbname=' . $config['database']['name'] . ';charset=utf8',
+    $config['database']['username'],
+    $config['database']['password'],
+    $config['database']['options']
+);
+$db = new QueryBuilder($pdo);
+$page3Content = $db->selectAll('page3')[0]->page3_text;
+?>
+
+<p><?php echo $page3Content; ?></p>
+
 <?php require('partials/footer.php') ?>
